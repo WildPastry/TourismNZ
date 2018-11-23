eaa13c86da4d8989e33dc2e5e51ef32e1e4da42b
WildPastry <mykdsn@gmail.com>
2018-11-24 10:00:54 +1300

    Tourism NZ

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
 /*jslint browser:true */
+$(document).ready(() => {
+
+    console.log('JS READY');
+// LOADER
+// $(window).on('load', function () {
+//     $('#status').fadeOut();
+//     $('#preloader').delay(350).fadeOut('slow');
+//     $('body').delay(350).css({
+//         'overflow': 'visible'
+//     });
+// })
+
+// LOADER
+$(document).ajaxStart(() => {
+    $("#ajaxLoading").fadeIn(transitionTime);
+    $("#mask").fadeIn(transitionTime);
+}).ajaxStop(() => {
+    $("#ajaxLoading").fadeOut(transitionTime);
+    $("#mask").fadeOut(transitionTime);
+});
+
+});
