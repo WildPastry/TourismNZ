bbf646d5f813a499da21970b8449f661856dddaf
WildPastry <mykdsn@gmail.com>
2018-11-24 12:23:41 +1300

    Updated files

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,54 @@
 /*jslint browser:true */
-$(document).ready(() => {
 
-    console.log('JS READY');
+// DOCUMENT READY
+console.log('JS READY');
+
+// IMMUTABLE VARIABLES
+// const transitionTime = 400;
+
 // LOADER
-// $(window).on('load', function () {
-//     $('#status').fadeOut();
-//     $('#preloader').delay(350).fadeOut('slow');
-//     $('body').delay(350).css({
-//         'overflow': 'visible'
+$(window).on('load', function () {
+    $('#preloader-icon').fadeOut('slow');
+    $('#preloader').delay(350).fadeOut('slow');
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+});
+
+// LOADER
+// $(document).ajaxStart(() => {
+//     $("#preLoading").fadeIn(transitionTime);
+//     $("#mask").fadeIn(transitionTime);
+// }).ajaxStop(() => {
+//     $("#preLoading").fadeOut(transitionTime);
+//     $("#mask").fadeOut(transitionTime);
+// });
+
+// LOADER
+// $('body').jmspinner();
+// $('body').jmspinner(false);
+
+// $(document).ready(function(){
+//     $(document).ajaxStart(function(){
+//         $("#preloader").css("display", "block");
 //     });
-// })
+//     $(document).ajaxComplete(function(){
+//         $("#preloader").css("display", "none");
+//     });
+// });
 
-// LOADER
-$(document).ajaxStart(() => {
-    $("#ajaxLoading").fadeIn(transitionTime);
-    $("#mask").fadeIn(transitionTime);
-}).ajaxStop(() => {
-    $("#ajaxLoading").fadeOut(transitionTime);
-    $("#mask").fadeOut(transitionTime);
-});
-
-});
+// function loadingAjax(div_id) {
+//     var divIdHtml = $("#"+div_id).html();
+//     $.ajax({
+//          type: "POST",
+//          url: "script.php",
+//          data: "name=John&id=28",
+//          beforeSend: function() {
+//             $("#loading-image").show();
+//          },
+//          success: function(msg) {
+//             $("#"+div_id).html(divIdHtml + msg);
+//             $("#loading-image").hide();
+//          }
+//     });
+// }
