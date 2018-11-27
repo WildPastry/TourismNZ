b01294ee320c332184b3a8f8c3919152ee09eeba
WildPastry <mykdsn@gmail.com>
2018-11-28 07:44:02 +1300

    Updated files

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,194 @@
 /*jslint browser:true */
 console.log('JS READY');
 
+// MAP
+var map;
+var yoobee = {
+    lat: -41.279178,
+    lng: 174.780331
+};
+var newMarker;
+var clickMarkerLocation;
+var radioOptions = document.getElementsByName("mode");
+
+function initMap() {
+
+    for (var i = 0; i < radioOptions.length; i++) {
+        radioOptions[i].addEventListener('change', function(){
+            var transportMode = getTransportMode();
+            if(clickMarkerLocation){
+                showDirections(clickMarkerLocation, transportMode);
+            }
+        });
+    }
+
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: yoobee,
+        zoom: 6,
+        scrollwheel: true,
+        disableDefaultUI: true,
+        styles: [
+            {
+                elementType: 'geometry',
+                stylers: [{color: '#242f3e'}]
+            },
+            {
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#242f3e'}]
+            },
+            {
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#746855'}]
+            },
+            {
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{color: '#263c3f'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#6b9a76'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{color: '#38414e'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#212a37'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#9ca5b3'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{color: '#746855'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#1f2835'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#f3d19c'}]
+            },
+            {
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{color: '#2f3948'}]
+            },
+            {
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{color: '#17263c'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#515c6d'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#17263c'}]
+            }
+        ]
+    });
+
+    // var marker = new google.maps.Marker({
+    //     position: yoobee,
+    //     map: map
+    // });
+
+    map.addListener('click', function(event) {
+        var transportMode = getTransportMode();
+        clickMarkerLocation = event.latLng;
+
+        showDirections(clickMarkerLocation, transportMode);
+    });
+
+    function showDirections(destinationLocation, transportMode){
+        if(directionsDisplay){
+            directionsDisplay.setMap(null);
+        }
+
+        var request = {
+            origin: yoobee,
+            destination: destinationLocation,
+            travelMode: transportMode
+        };
+        directionsService.route(request, function(result, status) {
+            if (status == 'OK') {
+                directionsDisplay.setMap(map);
+                directionsDisplay.setDirections(result);
+                document.getElementById('time').innerText = 'Time to get to destination is '+ result.routes[0].legs[0].duration.text;
+                document.getElementById('distance').innerText = 'Total distance to destination is '+result.routes[0].legs[0].distance.text;
+                addMarker();
+            } else if(status == 'NOT_FOUND'){
+                document.getElementById('time').innerText = '';
+                document.getElementById('distance').innerText = '';
+                removeMarker();
+                alert("At least one of the locations specified in the request's origin, destination could not be geocoded.");
+            } else if(status == 'ZERO_RESULTS'){
+                document.getElementById('time').innerText = '';
+                document.getElementById('distance').innerText = '';
+                removeMarker();
+                alert("No route could be found between the origin and destination.");
+            }
+        });
+    }
+}
+
+function getTransportMode(){
+    if (radioOptions) {
+        for (var i = 0; i < radioOptions.length; i++) {
+            if (radioOptions[i].checked){
+                 return radioOptions[i].value;
+            }
+        }
+    }
+}
+
+function addMarker(){
+    removeMarker();
+
+    newMarker = new google.maps.Marker({
+        position: clickMarkerLocation,
+        map: map
+    });
+}
+
+function removeMarker(){
+    if(newMarker && newMarker.setMap){
+        newMarker.setMap(null);
+    }
+}
 // function initialize() {
 //     var startPoint = document.getElementById('startPoint');
 //     new google.maps.places.Autocomplete(startPoint);
@@ -94,7 +282,7 @@ $(document).ready(function () {
         showActiveTooltip: true,
         navigation: true,
         navigationPosition: 'left',
-        navigationTooltips: ['INDEX', 'PLAN', 'VIEW', 'SELECT', 'OPTIONS', 'CONFIRM'],
+        navigationTooltips: ['INDEX', 'PLAN', 'VIEW', 'MAP', 'OPTIONS', 'CONFIRM'],
     });
 });
 
@@ -115,243 +303,6 @@ $(function () {
 // console.log(document.getElementById('startPoint'));
 
 // google.maps.event.addDomListener(window, 'load', initialize);
-
-// MAP
-// var map;
-// var yoobee = {
-//     lat: -41.279178,
-//     lng: 174.780331
-// };
-// var newMarker;
-// var clickMarkerLocation;
-// var radioOptions = document.getElementsByName("mode");
-
-// function initialize() {
-
-//     for (var i = 0; i < radioOptions.length; i++) {
-//         radioOptions[i].addEventListener('change', function () {
-//             var transportMode = getTransportMode();
-//             if (clickMarkerLocation) {
-//                 showDirections(clickMarkerLocation, transportMode);
-//             }
-//         });
-//     }
-
-//     var directionsService = new google.maps.DirectionsService();
-//     var directionsDisplay = new google.maps.DirectionsRenderer();
-
-//     console.log(document.getElementById('map'));
-
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         center: yoobee,
-//         zoom: 6,
-//         scrollwheel: false,
-//         disableDefaultUI: true,
-//         styles: [{
-//                 elementType: 'geometry',
-//                 stylers: [{
-//                     color: '#242f3e'
-//                 }]
-//             },
-//             {
-//                 elementType: 'labels.text.stroke',
-//                 stylers: [{
-//                     color: '#242f3e'
-//                 }]
-//             },
-//             {
-//                 elementType: 'labels.text.fill',
-//                 stylers: [{
-//                     color: '#746855'
-//                 }]
-//             },
-//             {
-//                 featureType: 'administrative.locality',
-//                 elementType: 'labels.text.fill',
-//                 stylers: [{
-//                     color: '#d59563'
-//                 }]
-//             },
-//             {
-//                 featureType: 'poi',
-//                 elementType: 'labels.text.fill',
-//                 stylers: [{
-//                     color: '#d59563'
-//                 }]
-//             },
-//             {
-//                 featureType: 'poi.park',
-//                 elementType: 'geometry',
-//                 stylers: [{
-//                     color: '#263c3f'
-//                 }]
-//             },
-//             {
-//                 featureType: 'poi.park',
-//                 elementType: 'labels.text.fill',
-//                 stylers: [{
-//                     color: '#6b9a76'
-//                 }]
-//             },
-//             {
-//                 featureType: 'road',
-//                 elementType: 'geometry',
-//                 stylers: [{
-//                     color: '#38414e'
-//                 }]
-//             },
-//             {
-//                 featureType: 'road',
-//                 elementType: 'geometry.stroke',
-//                 stylers: [{
-//                     color: '#212a37'
-//                 }]
-//             },
-//             {
-//                 featureType: 'road',
-//                 elementType: 'labels.text.fill',
-//                 stylers: [{
-//                     color: '#9ca5b3'
-//                 }]
-//             },
-//             {
-//                 featureType: 'road.highway',
-//                 elementType: 'geometry',
-//                 stylers: [{
-//                     color: '#746855'
-//                 }]
-//             },
-//             {
-//                 featureType: 'road.highway',
-//                 elementType: 'geometry.stroke',
-//                 stylers: [{
-//                     color: '#1f2835'
-//                 }]
-//             },
-//             {
-//                 featureType: 'road.highway',
-//                 elementType: 'labels.text.fill',
-//                 stylers: [{
-//                     color: '#f3d19c'
-//                 }]
-//             },
-//             {
-//                 featureType: 'transit',
-//                 elementType: 'geometry',
-//                 stylers: [{
-//                     color: '#2f3948'
-//                 }]
-//             },
-//             {
-//                 featureType: 'transit.station',
-//                 elementType: 'labels.text.fill',
-//                 stylers: [{
-//                     color: '#d59563'
-//                 }]
-//             },
-//             {
-//                 featureType: 'water',
-//                 elementType: 'geometry',
-//                 stylers: [{
-//                     color: '#17263c'
-//                 }]
-//             },
-//             {
-//                 featureType: 'water',
-//                 elementType: 'labels.text.fill',
-//                 stylers: [{
-//                     color: '#515c6d'
-//                 }]
-//             },
-//             {
-//                 featureType: 'water',
-//                 elementType: 'labels.text.stroke',
-//                 stylers: [{
-//                     color: '#17263c'
-//                 }]
-//             }
-//         ]
-//     });
-
-//     // TURN LABELS OFF
-//     // {
-//     //     featureType: "all",
-//     //     elementType: "labels",
-//     //     stylers: [
-//     //       { visibility: "off" }
-//     //     ]
-//     //   }
-
-//     console.log(map);
-
-//     var marker = new google.maps.Marker({
-//         position: yoobee,
-//         map: map
-//     });
-
-//     map.addListener('click', function (event) {
-//         var transportMode = getTransportMode();
-//         clickMarkerLocation = event.latLng;
-
-//         showDirections(clickMarkerLocation, transportMode);
-//     });
-
-//     function showDirections(destinationLocation, transportMode) {
-//         if (directionsDisplay) {
-//             directionsDisplay.setMap(null);
-//         }
-
-//         var request = {
-//             origin: yoobee,
-//             destination: destinationLocation,
-//             travelMode: transportMode
-//         };
-//         directionsService.route(request, function (result, status) {
-//             if (status == 'OK') {
-//                 directionsDisplay.setMap(map);
-//                 directionsDisplay.setDirections(result);
-//                 document.getElementById('time').innerText = 'Time to get to destination is ' + result.routes[0].legs[0].duration.text;
-//                 document.getElementById('distance').innerText = 'Total distance to destination is ' + result.routes[0].legs[0].distance.text;
-//                 addMarker();
-//             } else if (status == 'NOT_FOUND') {
-//                 document.getElementById('time').innerText = '';
-//                 document.getElementById('distance').innerText = '';
-//                 removeMarker();
-//                 alert("At least one of the locations specified in the request's origin, destination could not be geocoded.");
-//             } else if (status == 'ZERO_RESULTS') {
-//                 document.getElementById('time').innerText = '';
-//                 document.getElementById('distance').innerText = '';
-//                 removeMarker();
-//                 alert("No route could be found between the origin and destination.");
-//             }
-//         });
-//     }
-// }
-
-// function getTransportMode() {
-//     if (radioOptions) {
-//         for (var i = 0; i < radioOptions.length; i++) {
-//             if (radioOptions[i].checked) {
-//                 return radioOptions[i].value;
-//             }
-//         }
-//     }
-// }
-
-// function addMarker() {
-//     removeMarker();
-
-//     newMarker = new google.maps.Marker({
-//         position: clickMarkerLocation,
-//         map: map
-//     });
-// }
-
-// function removeMarker() {
-//     if (newMarker && newMarker.setMap) {
-//         newMarker.setMap(null);
-//     }
-// }
 
 // VIDEO
 // document.getElementById('brand-title').addEventListener('click', hideVideo);
@@ -471,3 +422,13 @@ $('.datepicker1').change(function () {
 //             }
 //         });
 // }
+
+// SELECT MENU
+// $( function() {
+
+//     $( "#peopleNum" )
+//       .selectmenu()
+//       .selectmenu( "menuWidget" )
+//         .addClass( "overflow" );
+ 
+//   } );
